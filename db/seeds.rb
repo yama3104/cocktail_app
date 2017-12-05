fbe73dd139a792d686ab52a2086ec76b7e4b1a13
yama3104 <yamaguchi3104786@yahoo.co.jp>
2017-12-05 06:58:52 +0000

    db追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -42,5 +42,71 @@ Cocktail.create(name: 'ソルティドッグ',
                mixer: 'グレープフルーツジュース',
                other: '')
 
+Cocktail.create(name: 'カンパリ・ビア',
+               base: 'ビール',
+               mixer: 'カンパリ',
+               other: '')
+
+Cocktail.create(name: 'スプリッツァー',
+               base: '白ワイン',
+               mixer: 'ソーダ',
+               other: '')
+
+Cocktail.create(name: 'キール',
+               base: '白ワイン',
+               mixer: 'カシスリキュール',
+               other: '')
+
+Cocktail.create(name: 'カーディナル',
+               base: '赤ワイン',
+               mixer: 'カシスリキュール',
+               other: '')
+
+Cocktail.create(name: 'キールロワイヤル',
+               base: 'シャンパン',
+               mixer: 'カシスリキュール',
+               other: '')
+
+Cocktail.create(name: 'キティ',
+               base: '赤ワイン',
+               mixer: 'ジンジャーエール',
+               other: '')
+
+Cocktail.create(name: 'オペレーター',
+               base: '白ワイン',
+               mixer: 'ジンジャーエール',
+               other: '')
+
+Cocktail.create(name: 'カルーアミルク',
+               base: 'カルーアリキュール',
+               mixer: '牛乳',
+               other: '')
+
+Cocktail.create(name: 'カンパリオレンジ',
+               base: 'カンパリ',
+               mixer: 'オレンジ',
+               other: '')
+  
+Cocktail.create(name: 'カンパリソーダ',
+               base: 'カンパリ',
+               mixer: 'ソーダ',
+               other: '')
+
+Cocktail.create(name: 'カシスオレンジ',
+               base: 'カシスリキュール',
+               mixer: 'オレンジ',
+               other: '')
+
+Cocktail.create(name: 'カシスソーダ',
+               base: 'カシスリキュール',
+               mixer: 'オレンジ',
+               other: '')
+
+Cocktail.create(name: 'ブラッディメアリー',
+               base: 'ウォッカ',
+               mixer: 'トマトジュース',
+               other: 'レモンジュース')
+
+
   
 
